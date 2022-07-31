df2a8d1c62958611513c38cef7663ecb726db587
rohsin47 <rohsin3247@gmail.com>
2022-07-31 14:16:57 +0200

    code value objects to interfaces.

--- src/main/java/com/flatex/watergate/immutables/Account.java
+++ src/main/java/com/flatex/watergate/immutables/Account.java
@@ -2,19 +2,18 @@ package com.flatex.watergate.immutables;
 
 import org.immutables.value.Value;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 /**
  * Default Implementation
  */
 @Value.Immutable
-@Value.Style(validationMethod = Value.Style.ValidationMethod.NONE)
-@Value.Modifiable
-public abstract class Account {
-
-    abstract int getId();
-    abstract String getType();
-    abstract String getCurrency();
-    abstract LocalDateTime getCreatedDate();
-
+@Value.Style(jdkOnly = true)
+public interface Account {
+    int getId();
+    String getType();
+    String getCurrency();
+    LocalDateTime getCreatedDate();
+    List<Entry> getEntries();
 }
